77a223a276dfa869d8b52d4fcc3ad295a383a221
unknown <james.r@kalvium.community>
2025-02-20 12:31:27 +0530

    cmplted Ca

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app= express();
+const PORT = 8080;
+
+const userData = [
+    {username:"james", age: 20 , email:"james@gmail.com"},
+    {username:"kevin", age: 20 , email:"kevin@gmail.com"},
+    {username:"aiwin", age: 20 , email:"aiwin@gmail.com"},
+    {username:"savio", age: 20 , email:"savio@gmail.com"},
+]
+
+app.get("/",(req,res)=>{
+    res.send("This is the backend")
+})
+
+app.get("/user",(req,res)=>{
+    const {email} = req.query;
+    if(!email){
+        return res.status(400).json({message:"email is required"})
+    }
+    const user =userData.find(user => user.email === email)
+    if(!user){
+        return res.status(404).json({message: "user not found"})
+    }
+    return res.status(200).json({message: "user found", user})
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log("The server is running on http://localhost:8080")
+})
